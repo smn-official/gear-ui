78e7e4ffcc2a9ed52fa2d52fb3b1d9dc38fa99a5
Mario Santos <mariocsants@gmail.com>
2017-04-28 13:39:13 -0300

    Implementando data minima e maxima na mascara de data

--- docs/src/app/core/core.controller.js
+++ docs/src/app/core/core.controller.js
@@ -11,12 +11,25 @@
         vm.ontem = new Date();
         vm.ontem.setHours(-1);
 
+        vm.users = [{
+            id: 1,
+            name: 'Jhon Doe'
+        }];
+
         activate();
 
         ////////////////
 
         function activate() {
             console.log('Core controller loaded with success');
+        }
+
+        vm.teste = function (string) {
+            console.log(string)
+        }
+        
+        vm.busca = function () {
+            
         }
     }
 
