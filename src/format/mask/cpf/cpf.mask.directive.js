0847c6bae0c260acd70a57b2988e54a07c0e2df8
wesley <wesley@smn.com.br>
2017-12-26 14:54:51 -0200

    Upload Ped-Left CPF

--- src/format/mask/cpf/cpf.mask.directive.js
+++ src/format/mask/cpf/cpf.mask.directive.js
@@ -14,17 +14,16 @@
         return directive;
 
         function link(scope, element, attrs, ctrl) {
-            ctrl.$parsers.push(function(value){
+            ctrl.$parsers.push(function (value) {
                 var viewValue = uiCpfFilter(value);
                 ctrl.$setViewValue(viewValue);
                 ctrl.$render();
-                if (viewValue.length === 14)
-                    return viewValue.replace(/[^0-9]+/g, '');
-                if (!viewValue)
-                    return '';
+                if (viewValue.length === 14) return viewValue.replace(/[^0-9]+/g, '');
+                if (!viewValue) return '';
             });
 
-            ctrl.$formatters.push(function(value){
+            ctrl.$formatters.push(function (value) {
+                if (value) value = ("00000000000" + value).substring((11 + value.length) - 11);
                 return uiCpfFilter(value);
             });
         }
