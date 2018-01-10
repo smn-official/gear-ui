dc539ad818ad00603a7edc13349bd330a659440c
wesley <wesley@smn.com.br>
2018-01-10 10:24:30 -0200

    Verificação se CPF e CNPJ são válidos

--- src/format/mask/cnpj/cnpj.mask.directive.js
+++ src/format/mask/cnpj/cnpj.mask.directive.js
@@ -24,6 +24,7 @@
                 afterSelIndex = doGetCaretPosition(element[0]);
 
                 var viewValue = uiCnpjFilter(value);
+                ctrl.$setValidity('cnpj', isValidCnpj(viewValue));
                 ctrl.$setViewValue(viewValue);
                 ctrl.$render();
 
@@ -96,6 +97,59 @@
                         }
                     }
                 }
+            }
+
+            function isValidCnpj(cnpj) {
+                if(!cnpj)return true;
+
+                if(cnpj.length >= 18) {
+                    var valid = true;
+                    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+                    if (cnpj.length != 14) valid = false;
+
+                    if (cnpj == "00000000000000" ||
+                        cnpj == "11111111111111" ||
+                        cnpj == "22222222222222" ||
+                        cnpj == "33333333333333" ||
+                        cnpj == "44444444444444" ||
+                        cnpj == "55555555555555" ||
+                        cnpj == "66666666666666" ||
+                        cnpj == "77777777777777" ||
+                        cnpj == "88888888888888" ||
+                        cnpj == "99999999999999") valid = false;
+
+                    var size = cnpj.length - 2;
+                    var numbers = cnpj.substring(0, size);
+                    var digits = cnpj.substring(size);
+                    var sum = 0;
+                    var pos = size - 7;
+                    for (var i = size; i >= 1; i--) {
+                        sum += numbers.charAt(size - i) * pos--;
+                        if (pos < 2)
+                            pos = 9;
+                    }
+                    var result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+                    if (result != digits.charAt(0))
+                        valid = false;
+
+                    size = size + 1;
+                    numbers = cnpj.substring(0, size);
+                    sum = 0;
+                    pos = size - 7;
+                    for (var i = size; i >= 1; i--) {
+                        sum += numbers.charAt(size - i) * pos--;
+                        if (pos < 2)
+                            pos = 9;
+                    }
+                    result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+                    if (result != digits.charAt(1))
+                        valid = false;
+
+                    if (!valid)
+                        return false;
+                }
+                return true;
             }
         }
     }
--- src/format/mask/cpf/cpf.mask.directive.js
+++ src/format/mask/cpf/cpf.mask.directive.js
@@ -16,6 +16,7 @@
         function link(scope, element, attrs, ctrl) {
             ctrl.$parsers.push(function (value) {
                 var viewValue = uiCpfFilter(value);
+                ctrl.$setValidity('cpf', isValidCpf(viewValue));
                 ctrl.$setViewValue(viewValue);
                 ctrl.$render();
                 if (viewValue.length === 14) return viewValue.replace(/[^0-9]+/g, '');
@@ -25,8 +26,51 @@
             ctrl.$formatters.push(function (value) {
                 value = typeof value == 'number' ? value.toString() : value;
                 if (value) value = ("00000000000" + value).substring((11 + value.length) - 11);
-                return uiCpfFilter(value);
+                    return uiCpfFilter(value);
             });
+
+            function isValidCpf(cpf) {
+                if(!cpf)return true;
+
+                if (cpf.length >= 14) {
+                    var valid = true;
+
+                    cpf = cpf.replace(/[^\d]+/g, '');
+
+                    if (cpf.length != 11) valid = false;
+
+                    var sum;
+                    var rest;
+                    sum = 0;
+                    if (cpf == "00000000000" ||
+                        cpf == "11111111111" ||
+                        cpf == "22222222222" ||
+                        cpf == "33333333333" ||
+                        cpf == "44444444444" ||
+                        cpf == "55555555555" ||
+                        cpf == "66666666666" ||
+                        cpf == "77777777777" ||
+                        cpf == "88888888888" ||
+                        cpf == "99999999999") valid = false;
+
+                    for (var i = 1; i <= 9; i++) sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+                    rest = (sum * 10) % 11;
+
+                    if ((rest == 10) || (rest == 11)) rest = 0;
+                    if (rest != parseInt(cpf.substring(9, 10))) valid = false;
+
+                    sum = 0;
+                    for (var i = 1; i <= 10; i++) sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+                    rest = (sum * 10) % 11;
+
+                    if ((rest == 10) || (rest == 11)) rest = 0;
+                    if (rest != parseInt(cpf.substring(10, 11))) valid = false;
+
+                    if (!valid)
+                        return false;
+                }
+                return true;
+            }
         }
     }
 
