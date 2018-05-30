2c0258f014fa49f4d9512dab2c8ed895d2e96284
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-05-30 09:47:41 -0300

    Externalizando textLabel do datalist

--- src/components/datalist/datalist.directive.js
+++ src/components/datalist/datalist.directive.js
@@ -1,50 +1,51 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular.module('smn-ui').directive('uiDatalist', ["$timeout", "$filter", function ($timeout, $filter) {
-		return {
-			restrict: 'AE',
-			transclude: true,
-			require: '?ngModel',
-			scope: {
-				'list': '=',
-				'idList': '@',
-				'placeholderList': '@',
-				'config': '=',
-				'attrList': '@',
-				'itemDefault': '@',
-				'labelList': '@',
-				'change': '=',
-				'click': '=',
-                'invalidValue': '@'
-			},
-			template: '<ui-input-container><input autocomplete="off" placeholder="{{placeholderList}}" ng-change="selected()" list="{{idList}}" ng-model="choosen"><datalist id="{{idList}}"><option ng-repeat="opt in list">{{opt[config.option]}}</option></datalist><label>{{labelList}}</label></ui-input-container>',
-			link: function link(scope, element, attrs, ngModel) {
-				scope.config = angular.extend(scope.config || {}, {
-					display: null,
-					option: 'nome',
-					value: 'id'
-				});
+    angular.module('smn-ui').directive('uiDatalist', ["$timeout", "$filter", function ($timeout, $filter) {
+        return {
+            restrict: 'AE',
+            transclude: true,
+            require: '?ngModel',
+            scope: {
+                'list': '=',
+                'idList': '@',
+                'placeholderList': '@',
+                'config': '=',
+                'attrList': '@',
+                'itemDefault': '@',
+                'labelList': '@',
+                'change': '=',
+                'click': '=',
+                'invalidValue': '@',
+                'modal': '=?'
+            },
+            template: '<ui-input-container><input autocomplete="off" placeholder="{{placeholderList}}" ng-change="selected()" list="{{idList}}" ng-model="modal"><datalist id="{{idList}}"><option ng-repeat="opt in list">{{opt[config.option]}}</option></datalist><label>{{labelList}}</label></ui-input-container>',
+            link: function link(scope, element, attrs, ngModel) {
+                scope.config = angular.extend(scope.config || {}, {
+                    display: null,
+                    option: 'nome',
+                    value: 'id'
+                });
 
-				scope.$watch('list', function () {
-					if (!scope.list) return;
-					var iDefault = scope.list.filter(function (obj) { return !!obj.default; });
-					if (scope.list && scope.itemDefault && !iDefault.length)
-						scope.list.splice(0, 0, angular.isObject(scope.list[0])
-							? { [scope.config.option]: scope.itemDefault, [scope.config.value]: null, default: true }
-							: scope.itemDefault);
-				});
+                scope.$watch('list', function () {
+                    if (!scope.list) return;
+                    var iDefault = scope.list.filter(function (obj) { return !!obj.default; });
+                    if (scope.list && scope.itemDefault && !iDefault.length)
+                        scope.list.splice(0, 0, angular.isObject(scope.list[0])
+                            ? { [scope.config.option]: scope.itemDefault, [scope.config.value]: null, default: true }
+                            : scope.itemDefault);
+                });
 
-				scope.selected = function () {
+                scope.selected = function () {
                     var itemSelected = scope.list.filter(function (obj) {
-                        return obj[scope.config.option] == scope.choosen;
+                        return obj[scope.config.option] == scope.modal;
                     })[0];
 
-                    var rtn = !scope.attrList ? itemSelected || null : itemSelected ? itemSelected[scope.attrList] : scope.invalidValue ? scope.choosen : null;
+                    var rtn = !scope.attrList ? itemSelected || null : itemSelected ? itemSelected[scope.attrList] : scope.invalidValue ? scope.modal : null;
                     ngModel.$setViewValue(rtn);
                     scope.change && scope.change(rtn);
                 };
-			}
-		};
-	}]);
+            }
+        };
+    }]);
 })();
